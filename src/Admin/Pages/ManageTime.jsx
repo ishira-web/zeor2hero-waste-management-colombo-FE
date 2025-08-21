3f042dcbe0d335d59c4d276c428442a1d8d5e3f8
ishira-web <ishira.pahasara@icloud.com>
2025-08-21 07:31:01 +0530

    refactor: clean up ManageTime component by removing unnecessary whitespace

--- src/Admin/Pages/ManageTime.jsx
+++ src/Admin/Pages/ManageTime.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function ManageTime() {
-    
+
   return (
     <div>
-       
+       {/* Using this get all collectors endpoint can get  all collectors  so for a team can assign a leader and his crew  all are collectors =    http://localhost:3000/api/collector/getcollectors */}
+       {/* This endpoint can create time table for a crew endpoint - http://localhost:3000/api/timetable/create */}
+        {/* This endpoint can get all time tables - http://localhost:3000/api/timetable/getall */}
+        {/* THis is get All Routes Endpoint .Youcan select one of them routes-   http://localhost:3000/api/route/all  */}
+        
+        
     </div>
   )
 }
